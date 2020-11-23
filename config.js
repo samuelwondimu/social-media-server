805ceedd38cb152ccc48c5cf1a2e620e17940301
samuel wondimu <wsamuelwondimu@gmail.com>
2020-11-23 19:39:14 +0300

    init

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB: 
+    'mongodb+srv://classed:HkRnZhRQRlEVgZjr@cluster0.jtmbg.mongodb.net/socialMedia?retryWrites=true&w=majority',
+  SECRET_KEY: "some very secret key"
+}
